a6044085d24b246f8ea40d7e4b35e57ec0d78937
Hunter Coffman <hcoffman@ucsc.edu>
2018-12-02 14:50:41 -0800

    Have the right bar working. Need Vue to incorporate dynamic post information

--- static/js/default_index.js
+++ static/js/default_index.js
@@ -1,3 +1,59 @@
+$(document).ready(function(e) {
+
+        //Variable instantiation.
+	var map_fullscreen = false;
+
+	var menu_width = "30%";
+	var map_width = "70%";
+	
+	var windowHeight = $(window).height();
+	var windowWidth = $(window).outerWidth(true);
+	console.log("Height: " + windowHeight);
+	console.log("Width: " + windowWidth);
+	
+
+	$("#menuid").css({
+            "width":menu_width
+	})
+	$("#mapid").css({
+            "width":map_width
+	})
+	$("#horiz_bar").css({
+	})
+
+	
+	/*$("#menu_button").click(function () {
+	    console.log("CLICK!");
+            $("#menu_button").animate({
+                "right":"500"
+	    }, 500, "easeOutBounce")
+	})*/
+
+
+        $("#menu_button2").click(function(){
+	    /*$("#menu_button").transition({
+	        "right":"500"
+	    },500,"easeInOutBack")*/
+	    if (map_fullscreen) {
+	        $("#menuid").css({
+                    "width":menu_width
+	        })
+	        $("#mapid").css({
+                    "width":map_width
+	        })
+	        map_fullscreen = false;
+	    } else {
+	        $("#menuid").css({
+                    "width":"0%"
+	        })
+	        $("#mapid").css({
+                    "width":"100%"
+	        })
+	        map_fullscreen = true;
+	    }
+	});
+});
+
 
 var app = function() {
 
@@ -12,7 +68,11 @@ var app = function() {
 
     // Enumerates an array.
     var enumerate = function(v) { var k=0; return v.map(function(e) {e._idx = k++;});};
-    
+
+    //FIXME: When vue is working, implement this and remove above method.
+    //self.press_menu_button = function () {
+    //    
+    //};
     
     self.vue = new Vue({
         el: "#vue-div",
